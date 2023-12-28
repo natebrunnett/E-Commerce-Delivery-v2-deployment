9cbf67a0e56a8e0f38c2270c654bbfa4586e3eb3
Nathan Brunnett <natebrunnett@gmail.com>
2023-12-28 16:01:41 +0100

    cyclic

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
-const URL = "http://localhost:3030"
+//const URL = "http://localhost:3030"
 
-//const URL = "YourCyclicProductionURL"
+const URL = "https://lonely-dog-gaiters.cyclic.app"
 
 export default URL
