4c9538f3c2f9de6d6354ce527bbe3348934ad2ce
Nathan Brunnett <natebrunnett@gmail.com>
2023-12-30 18:26:54 +0100

    removed adminbro

--- server/app.js
+++ server/app.js
@@ -25,28 +25,28 @@ mongoose.set('strictQuery', false);
 // npm i adminjs @adminjs/express @adminjs/mongoose  tslib express-formidable express-session
 
 // require adminjs
-const AdminJS = require('adminjs')
-// require express plugin
-const AdminJSExpress = require('@adminjs/express')
-// require mongoose adapter
-AdminJS.registerAdapter(require("@adminjs/mongoose"));
-// Import all the project's models
-const Customers = require("./models/Customer.js"); // replace this for your model
-const Products = require("./models/Products.js"); // replace this for your model
+// const AdminJS = require('adminjs')
+// // require express plugin
+// const AdminJSExpress = require('@adminjs/express')
+// // require mongoose adapter
+// AdminJS.registerAdapter(require("@adminjs/mongoose"));
+// // Import all the project's models
+// const Customers = require("./models/Customer.js"); // replace this for your model
+// const Products = require("./models/Products.js"); // replace this for your model
 
-const UsersAdmin = require("./admin/resource_options/users.admin"); // replace this for your model
-const buildAdminRouter = require("./admin/admin.router");
+// const UsersAdmin = require("./admin/resource_options/users.admin"); // replace this for your model
+// const buildAdminRouter = require("./admin/admin.router");
 
-const adminOptions = new AdminJS({
-  resources: [Customers, Products, UsersAdmin], // the models must be included in this array
-  rootPath: "/admin"
-});
+// const adminOptions = new AdminJS({
+//   resources: [Customers, Products, UsersAdmin], // the models must be included in this array
+//   rootPath: "/admin"
+// });
 
-// initialize adminjs 
-const admin = new AdminJS(adminOptions,buildAdminRouter)
-// build admin route
-const router = buildAdminRouter(admin);
-app.use(admin.options.rootPath, router);
+// // initialize adminjs 
+// const admin = new AdminJS(adminOptions,buildAdminRouter)
+// // build admin route
+// const router = buildAdminRouter(admin);
+// app.use(admin.options.rootPath, router);
 // end ADMINJS
 
 
